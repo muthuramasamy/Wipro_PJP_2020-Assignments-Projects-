6535ae24a60f9dde163c6365667dd9924dab9f70
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:40:47 +0530

    Create Assignment_1_(2).java

---
+++ 3_Abstraction_exception_Package/Abstraction/RailInstrument/Assignment_1_(2).java
@@ -0,0 +1,13 @@
+package Abstraction_compartment_rail;
+/**
+ * 
+ * @author T.MUTHU SUNDARAM
+ *
+ */
+public class first_compartment extends Compartment {
+public String notice()
+{
+	String s = "This is first class compartment and used to carry reserved tickets..!";
+	return s;
+
+}}
