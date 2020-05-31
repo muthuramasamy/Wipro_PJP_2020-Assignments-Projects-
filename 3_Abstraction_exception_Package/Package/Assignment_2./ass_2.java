7eeed37fe3f01a4fa43ac4e9c687b51c3265765c
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:57:22 +0530

    Create ass_2.java

---
+++ 3_Abstraction_exception_Package/Package/Assignment_2./ass_2.java
@@ -0,0 +1,7 @@
+package Automobile_sample2;
+
+public abstract class Vehicle {
+	public abstract String getModelNumber();
+	public abstract String getRegistrationNumber();
+	public abstract String getOwnerName();
+}
