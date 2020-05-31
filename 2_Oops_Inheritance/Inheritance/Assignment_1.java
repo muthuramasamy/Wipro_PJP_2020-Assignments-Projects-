84b4016ad93c895fdd19f7e257f0083fde51e369
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:10:45 +0530

    Create Assignment_1.java

---
+++ 2_Oops_Inheritance/Inheritance/Assignment_1.java
@@ -0,0 +1,14 @@
+/**
+	 * @author MUTHUSUNDARAM_T
+	 */
+public class Animal {
+	public void eat()
+	{
+	System.out.println("All the animals can eat..!");	
+	}
+	public void sleep()
+	{
+		System.out.println("All the animals can sleep..!");
+	}
+
+}
