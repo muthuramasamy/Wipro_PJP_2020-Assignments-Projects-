e1d42644086a3f36944c87849df66e601a8ec723
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:14:52 +0530

    Create Assignment_3_(2).java

---
+++ 2_Oops_Inheritance/Inheritance/Assignment_3_(2).java
@@ -0,0 +1,14 @@
+/**
+	 * @Author MUTHUSUNDARAM_T
+	 */
+public class Student extends anotherperson {
+	int stuId;
+	public Student(String name, String dob,int stuId) {
+		super(name, dob);
+		this.stuId= stuId;
+	}
+	
+	
+
+
+}
