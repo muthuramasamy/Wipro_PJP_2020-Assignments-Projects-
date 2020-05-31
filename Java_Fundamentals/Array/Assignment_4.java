2b3ac7df0a258e415c18debc5769490cf1029010
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:03:07 +0530

    Create Assignment_4.java

---
+++ Java_Fundamentals/Array/Assignment_4.java
@@ -0,0 +1,17 @@
+package array;
+
+public class ascii {
+
+	/**
+	 * @author Muthu sundaram
+	 */
+	public static void main(String[] args) {
+		int [] array = new int[]{66,68,59,100};
+		for (int i : array)
+		{
+			System.out.println((char)i+" ");
+		}
+
+	}
+
+}
