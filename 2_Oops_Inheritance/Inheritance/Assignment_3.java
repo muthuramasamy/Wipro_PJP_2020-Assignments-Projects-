77b7ae07421bd7bcf088b8f050be9ff694ce37c8
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:13:46 +0530

    Create Assignment_3.java

---
+++ 2_Oops_Inheritance/Inheritance/Assignment_3.java
@@ -0,0 +1,25 @@
+/**
+	 * @Author MUTHUSUNDARAM_T
+	 */
+public class anotherperson {
+
+	String name;
+	String dob;
+	public anotherperson(String name, String dob) {
+		this.name=name;
+		this.dob=dob;
+	}
+	public void getname()
+	{
+		this.name=name;
+	}
+	public void getdob()
+	{
+		this.dob=dob;
+	}
+	
+
+	
+	
+
+}
