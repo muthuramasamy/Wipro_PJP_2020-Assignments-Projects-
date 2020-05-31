d97fee8daf356320e7af9cceeecba32cdcfbf4b3
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:20:37 +0530

    Create Assignment_2_(2).java

---
+++ 2_Oops_Inheritance/Polymorphism/Assignment_2_(2).java
@@ -0,0 +1,16 @@
+package Polymorphism;
+/**
+ * 
+ * @author MUTHUSUNDARAM_T
+ *
+ */
+public class shapes_triangle extends shapes {
+	public void draw()
+	{
+		System.out.println("Drawing triangle");
+	}
+	public void erase()
+	{
+		System.out.println("Erasing triangle");
+	}
+}
