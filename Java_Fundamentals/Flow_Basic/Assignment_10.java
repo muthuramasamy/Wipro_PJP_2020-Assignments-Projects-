68cd932e4352b4c54e913aaf741a3e5a515c1b95
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 19:54:34 +0530

    Create Assignment_10.java

---
+++ Java_Fundamentals/Flow_Basic/Assignment_10.java
@@ -0,0 +1,16 @@
+package flowbasic;
+
+public class one_to_ten_single_row {
+
+	/**
+	 * @author muthu sundaram
+	 */
+	public static void main(String[] args) {
+		for (int i=1;i<=10;i++)
+		{
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
