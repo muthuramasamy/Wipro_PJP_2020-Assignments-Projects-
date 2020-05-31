fc318ab64cac1b076d2fb739b56cada1e3f17482
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:21:04 +0530

    Create Assignment_2_(3).java

---
+++ 2_Oops_Inheritance/Polymorphism/Assignment_2_(3).java
@@ -0,0 +1,16 @@
+package Polymorphism;
+/**
+ * 
+ * @author MUTHUSUNDARAM_T
+ *
+ */
+public class shapes_circle extends shapes {
+	public void draw()
+	{
+		System.out.println("Drawing circle");
+	}
+	public void erase()
+	{
+		System.out.println("Erasing circle");
+	}
+}
