a4d41abe185b40b34d4b65b34232003dde9a528b
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:55:44 +0530

    Create ass_1_(1).java

---
+++ 3_Abstraction_exception_Package/Package/Assignment_1/ass_1_(1).java
@@ -0,0 +1,17 @@
+package Packages;
+/**
+ * 
+ * @author T.MUTHU SUNDARAM
+ *
+ */
+import java.util.*;
+import java.io.*;
+public class foundation {
+
+		private int variable1= 10;
+		int variable2 = 20;
+		public int variable3 = 30;
+		protected int variable4 = 40;
+
+
+}
