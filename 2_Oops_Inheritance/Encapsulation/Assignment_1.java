5e56fc1d4614fe2bca09fb182b68354c2df2a7a8
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:08:34 +0530

    Create Assignment_1.java

---
+++ 2_Oops_Inheritance/Encapsulation/Assignment_1.java
@@ -0,0 +1,21 @@
+package Encapsulation_Abstraction;
+/**
+ * 
+ * @author MUTHUSUNDARAM_T
+ *
+ */
+import java.util.*;
+import java.io.*;
+public class Author {
+String name;
+String email;
+char gender ; 
+Author(String name,String email,char gender)
+{
+this.email =email;
+this.name = name;
+this.gender= gender;
+}
+	
+
+}
