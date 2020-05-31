2050873e80de87af4d7ea4dd804fd0adc7cb68e8
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:55:24 +0530

    Create ass_1.java

---
+++ 3_Abstraction_exception_Package/Package/Assignment_1/ass_1.java
@@ -0,0 +1,21 @@
+package test_package;
+/**
+ * 
+ * @author T.MUTHU SUNDARAM
+ *
+ */
+import java.util.*;
+import java.io.*;
+import Packages.foundation;
+public class test_pack {
+
+	public static void main(String[] args) {
+		foundation d = new foundation();
+		//System.out.println(d.variable1);
+		//System.out.println(d.variable2);
+		System.out.println(d.variable3); //Due to it initialization as public
+		
+		//System.out.println(d.variable4);
+	}
+
+}
