628e7c3a7c2a351cd03e84c7c8b5ac87046be869
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 21:31:54 +0530

    Update Assignment_1.java

--- 8_RDBMS_JDBC_SQL/Callable_statement/Assignment_1.java
+++ 8_RDBMS_JDBC_SQL/Callable_statement/Assignment_1.java
@@ -1,28 +1,55 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
 
-package List;
-/**
- * 
- * @author MUTHUSUNDARAM_T
- *
- */
-import java.util.ArrayList;
-import java.util.Scanner;
+public class Callable1 {
 
-public class Mixed_Array {
-	
-	public static void main(String[] args) {
-		ArrayList<Datatype> al = new ArrayList<Datatype>();
-		
-		Scanner sc = new Scanner(System.in);
-		int input1 =sc.nextInt();
-		float input2 = sc.nextFloat();
-		double input3 = sc.nextDouble();
-		Datatype dta = new Datatype();
-		dta.addint(input1);
-		dta.addfloat(input2);
-		dta.adddouble(input3);
+    public static void main(String[] args) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        CallableStatement cstmt = null;
 
-		al.add(dta);
-	}
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521", "abhishek", "wipro-pjp");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String sql = "SELECT * FROM emp";
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                int empno = rs.getInt("empno");
+                String ename = rs.getString("ename");
+                double netSalary = 0;
+
+                String sql2 = "{ call calculate_salary(?, ?) }";
+
+                cstmt = conn.prepareCall(sql2);
+                cstmt.setInt(1, empno);
+                cstmt.registerOutParameter(2, Types.DOUBLE);
+                cstmt.executeQuery();
+
+                netSalary = cstmt.getDouble(2);
+
+                System.out.println("empno: " + empno + ", ename: " + ename + ", net salary: " + netSalary);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error");
+            stmt.close();
+            cstmt.close();
+            conn.close();
+        }
+    }
 
 }
