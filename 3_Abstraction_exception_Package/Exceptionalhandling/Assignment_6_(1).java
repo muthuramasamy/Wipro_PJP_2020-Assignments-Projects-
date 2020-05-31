7af7e5d95063660e58bc09fafbfc7f37531b3355
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:49:04 +0530

    Create Assignment_6_(1).java

---
+++ 3_Abstraction_exception_Package/Exceptionalhandling/Assignment_6_(1).java
@@ -0,0 +1,8 @@
+package Exception_handle;
+
+public class ValueOutOfRange extends Exception{
+public String ValueOutOfRange()
+{
+	return "ValuesOut of Range Exception occurs..../Enter value between 1 - 100";
+	}
+}
