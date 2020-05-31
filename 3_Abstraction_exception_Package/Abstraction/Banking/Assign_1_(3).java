f331b91d6e4910155cb7beea2ca07ffcad2a44c2
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:33:16 +0530

    Create Assign_1_(3).java

---
+++ 3_Abstraction_exception_Package/Abstraction/Banking/Assign_1_(3).java
@@ -0,0 +1,17 @@
+package Abstraction;
+/**
+ * 
+ * @author T.MUTHUSUNDARAM
+ *
+ */
+public   class KOTAKbank extends Generalbank{
+	double getsavingsInterestrate()
+	{
+		return 4.5;
+	}
+	double getfixeddepositInterestrate()
+	{
+		return 9.2;
+	}
+	
+}
