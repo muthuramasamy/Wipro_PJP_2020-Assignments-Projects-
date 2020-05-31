19facfda46cc958571bf5339e8034df2332730f3
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:57:54 +0530

    Create ass_3.java

---
+++ 3_Abstraction_exception_Package/Package/Assignment_2./ass_3.java
@@ -0,0 +1,29 @@
+package Fourwheeler_l;
+
+import Automobile_sample2.Vehicle;
+public class LAMBOGINI extends Vehicle{
+
+	@Override
+	public String getModelNumber() {
+		return "LA !@#23";
+	}
+
+	@Override
+	public String getRegistrationNumber() {
+		return "TN 28 m 283";
+	}
+	@Override
+	public String getOwnerName() {
+	
+		return "AMIRTHAMATHI";
+	}
+	public int speed()
+	{
+		return 120;
+	}
+	public void radiotep()
+	{
+		System.out.println("The Air Condition false");
+	}
+
+}
