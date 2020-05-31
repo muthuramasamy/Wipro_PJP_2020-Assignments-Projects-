cabf225283a17d47b59f1b43b7eccc4cea8942b3
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:45:13 +0530

    Create Assignment_2.java

---
+++ 3_Abstraction_exception_Package/Exceptionalhandling/Assignment_2.java
@@ -0,0 +1,33 @@
+package Exception_handle;
+
+/*
+*@author MUTHUSUNDARAM_T
+*/
+import java.util.Scanner;
+
+public class element_check {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the number of elements to be stored in the array");
+		int num_consist = sc.nextInt();
+		int [] arr= new int[num_consist];
+		System.out.println("Enter the array elements : ");
+		for(int i=0;i<arr.length;i++)
+		{
+			 arr[i] = sc.nextInt();
+		}
+		try
+		{
+		System.out.println("Enter the index of the array element you want to access");
+		int user_ind = sc.nextInt();
+		System.out.println("The array element : " +user_ind+" is "+arr[user_ind]);
+		System.out.println("The element cane be accesed successfully..!");
+		}
+		catch(ArrayIndexOutOfBoundsException ae)
+		{
+			System.out.println("The class element_check creating ArrayindexOutOfNoundsException ");
+		}
+		}
+
+}
