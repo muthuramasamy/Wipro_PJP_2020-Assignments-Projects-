7693cfd8045f68e3641ad82263c5ed18da475b52
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 19:44:03 +0530

    Create Asignment2.java

---
+++ Java_Fundamentals/Language_Basics/Asignment2.java
@@ -0,0 +1,18 @@
+package language_Basics;
+
+import java.util.Scanner;
+
+public class string_concat {
+
+	/**
+	 * @author MUTHU SUNDARAM 
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		String s1 = sc.nextLine();
+		System.out.println(" Welcome "+ s1 );
+		sc.close();
+		
+	}
+
+}
