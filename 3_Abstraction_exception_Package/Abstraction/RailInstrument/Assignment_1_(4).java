1ee20720b1aea0c0226ad267767ad1cfaf50beac
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:42:11 +0530

    Create Assignment_1_(4).java

---
+++ 3_Abstraction_exception_Package/Abstraction/RailInstrument/Assignment_1_(4).java
@@ -0,0 +1,16 @@
+package Abstraction_compartment_rail;
+/**
+ * 
+ * @author T.MUTHU SUNDARAM
+ *
+ */
+public class luggage extends Compartment {
+	 public String notice()
+	{
+		String s = "This is luggage compartment and used to carry goods..!";
+		return s;
+		}
+
+	
+
+}
