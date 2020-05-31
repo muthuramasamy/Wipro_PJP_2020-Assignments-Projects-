d3ee254bdd5245b9b0bb078ca453c0a8052dfafd
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:52:14 +0530

    Create Assign_1_(3).java

---
+++ 3_Abstraction_exception_Package/Interface/Assign_1_(3).java
@@ -0,0 +1,13 @@
+package Interface_Instrumentplay;
+/*
+ * @author MUTHUSUNDARAM_T
+ */
+public class saxo implements playable {
+
+	@Override
+	public void play() {
+		System.out.println("they gets from switzerland");
+		
+	}
+
+}
