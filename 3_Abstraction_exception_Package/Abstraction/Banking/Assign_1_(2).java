de5045f4f6a71a37cc1197e07911b1a7a9955c16
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:32:25 +0530

    Create Assign_1_(2).java

---
+++ 3_Abstraction_exception_Package/Abstraction/Banking/Assign_1_(2).java
@@ -0,0 +1,17 @@
+package Abstraction;
+/**
+ * 
+ * @author T.MUTHUSUNDARAM
+ *
+ */
+public   class Icici extends Generalbank{
+	double getsavingsInterestrate()
+	{
+		return 4;
+	}
+	double getfixeddepositInterestrate()
+	{
+		return 8.5;
+	}
+	
+}
