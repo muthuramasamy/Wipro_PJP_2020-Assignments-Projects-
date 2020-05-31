e794a1eb40e5f5a97adb8938fbe3433572d253de
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:31:16 +0530

    Create Assign_1.java

---
+++ 3_Abstraction_exception_Package/Abstraction/Banking/Assign_1.java
@@ -0,0 +1,11 @@
+package Abstraction;
+/**
+ * @author muthu sundaram
+ */
+public abstract class Generalbank {
+
+	abstract double getsavingsInterestrate();
+	abstract double getfixeddepositInterestrate();
+	
+
+}
