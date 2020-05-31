76f7c7d9a6a8f172620e17a5f28f3b3ae2d97f48
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:54:51 +0530

    Create Assignment_3.java

---
+++ 5_Wrapper_class/Assignment_3.java
@@ -0,0 +1,36 @@
+/*
+Define a java class that accepts an integer(between 1 and 255) from the user and displays 
+the String representation of the argument passed as an unsigned integer in base 2. 
+The output displayed should contain 8 digits and should be padded with leading 0s(zeros), 
+in case the returned String contains less than 8 characters.
+For example, if the user enters the value 16, then the output should be
+00010000
+and if the user enters the value 100, the output should be
+01100100
+You are expected to use Integer class conversion method/s described in the PDF file. 
+Use Scanner class to accept user inputs.(Hint : You may use String.format() method for the expected output)
+ * */
+
+
+
+
+
+
+
+package wrapping;
+/*
+*Author Muthusundaram
+*/
+import java .util.*;
+public final class leading0_int_to_binary_8digits {
+	public static void main(String[] args) {
+		Scanner get_inpt = new Scanner(System.in);
+		int main_input = get_inpt.nextInt();
+		String output = String.format("%8s",Integer.toBinaryString(main_input)).replace(" ", "0");
+		System.out.println(output);
+		
+		
+		
+	}
+
+}
