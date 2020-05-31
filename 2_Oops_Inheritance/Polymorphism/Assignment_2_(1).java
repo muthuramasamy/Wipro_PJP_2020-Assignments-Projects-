ef0b22a4d2dc2f2a91fa963138823b419b605aff
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:19:57 +0530

    Create Assignment_2_(1).java

---
+++ 2_Oops_Inheritance/Polymorphism/Assignment_2_(1).java
@@ -0,0 +1,17 @@
+package Polymorphism;
+/**
+ * 
+ * @author MUTHUSUNDARAM_T
+ *
+ */
+public class shapes {
+	public void draw()
+	{
+		System.out.println("Drawing shapes");
+	}
+	public void erase()
+	{
+		System.out.println("Erasing shape");
+	}
+
+}
