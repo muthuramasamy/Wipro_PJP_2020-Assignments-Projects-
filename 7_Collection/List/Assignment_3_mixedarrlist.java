69a7084f05140150c83304ef2aafad754a7d21b5
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 21:20:01 +0530

    Create Assignment_3_mixedarrlist.java

---
+++ 7_Collection/List/Assignment_3_mixedarrlist.java
@@ -0,0 +1,25 @@
+package List;
+/**
+ * 
+ * @author MUTHUSUNDARAM_T
+ *
+ */
+public class Datatype {
+
+	int a ;
+	float b;
+	double c;
+	public void addint(int num)
+	{
+		a=num;
+	}
+	public void adddouble(double num)
+	{
+		c=num;
+	}
+	public void addfloat(float num)
+	{
+		b=num;
+	}
+
+}
