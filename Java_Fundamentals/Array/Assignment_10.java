8488da0b37b3dde49a546b0486a8cb2fcaf05c43
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:00:06 +0530

    Create Assignment_10.java

---
+++ Java_Fundamentals/Array/Assignment_10.java
@@ -0,0 +1,45 @@
+package array;
+import java.util.*;
+import java.io.*;
+public class Three_dimensional_arr_max {
+
+	/**
+	 * @Author Muthu sundaram
+	 */
+	 public static void main(String[] args) {
+			if(args.length == 9) {
+				
+				int arr[][] = new int[3][3];
+				int c = 0;
+				
+				for(int i = 0; i < 3; ++i) {
+					for(int j = 0; j < 3; ++j) {
+						arr[i][j] = Integer.parseInt(args[c]);
+						++c;
+					}
+				}
+				System.out.println("The given array is :");
+				for(int i = 0; i < 3; ++i) {
+					for(int j = 0; j < 3; ++j) {
+						System.out.print(arr[i][j]+" ");
+					}
+					System.out.println();
+				}
+				
+				int[] a = new int[9];
+				
+				for(int i = 0; i < 9; ++i) { 
+					a[i] = Integer.parseInt(args[i]);
+				}
+				
+				Arrays.sort(a);
+				int max_number = a[a.length-1];
+				System.out.println("The biggest number in the given array is " + max_number);
+			}
+			
+			else {
+				System.out.println("Please enter 9 integer numbers");
+			}
+		
+		 }
+	}
