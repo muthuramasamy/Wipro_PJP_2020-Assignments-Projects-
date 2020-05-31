5e5ac40769171222feab81051e979662b396b09b
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:15:56 +0530

    Create Assignment_3_(4).java

---
+++ 2_Oops_Inheritance/Inheritance/Assignment_3_(4).java
@@ -0,0 +1,16 @@
+/**
+	 * @Author MUTHUSUNDARAM_T
+	 */
+public class Test_tea_stu {
+	public static void main(String[] args)
+	{
+		college_stu cs = new college_stu(" MUTHU "," 16-09-1998 " ,  1310 ,  4, " JPR ");
+		cs.show();
+		System.out.println("-----------------------------------");
+		Teacher t = new Teacher(" AMIRTHAMATHI "," 02-05-2000", 100000.00, " COLECTOR ");
+		t.show();
+	}
+
+	
+
+}
