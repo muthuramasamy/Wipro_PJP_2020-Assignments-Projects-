2c9498784092a0a57c897f7485d824d635a8fa4f
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 20:12:53 +0530

    Create Assignment_2_(2).java

---
+++ 2_Oops_Inheritance/Inheritance/Assignment_2_(2).java
@@ -0,0 +1,12 @@
+
+public class Test_employee {
+	/**
+	 * @author MUTHUSUNDARAM_T
+	 */
+	public static void main(String[] args) {
+		employee e1 = new employee(" MUTHU " ," InS23-@34 ",  2005 , 30000.00);
+		e1.display();
+		
+	}
+
+}
