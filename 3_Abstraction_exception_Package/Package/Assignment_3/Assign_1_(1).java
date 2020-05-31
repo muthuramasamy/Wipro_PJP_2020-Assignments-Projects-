225dc24d0048057bff0da7eb06c7dde183048895
Muthu sundaram Ramasamy <muthubrutus1998@gmail.com>
2020-05-31 23:59:40 +0530

    Create Assign_1_(1).java

---
+++ 3_Abstraction_exception_Package/Package/Assignment_3/Assign_1_(1).java
@@ -0,0 +1,8 @@
+package automobile_package;
+
+public abstract class Vehicle {
+	abstract public  String getModelName() ;
+	abstract public String getRegistrationNumber() ;
+	abstract public String getOwnerName() ;
+
+}
